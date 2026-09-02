5f3338c7346c1b1c581fc0e1e8e1d2897719aa79
Utkarsha Chakrabortty <utkarshachakrabortty56@gmail.com>
2026-09-02 20:03:35 +0530

    Add Clerk webhook handler and integrate with Express

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,8 @@ import fs from "fs"
 import path from "path"
 import { pathToFileURL } from "url";
 import { nextTick } from "process";
+import job from "./lib/cron.js"
+import clerkWebhook from "./webhooks/clerk.webhook.js"
 
 const app = express();
 
@@ -15,6 +17,8 @@ const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
 const publicDir = path.join(process.cwd(), "public")
+
+app.use("/api/webhooks/clerk",express.raw({type: "application/json"}), clerkWebhook)
 
 app.use(express.json())
 app.use(cors({ origin: FRONTEND_URL, credentials:true}));
